c828d8d891d54021a40387c7b25ede8f2f0dbea7
0hmza <hmza04@icloud.com>
2025-11-25 19:30:34 +0100

    sa

--- rendu/rotone1/rotone.c
+++ rendu/rotone1/rotone.c
@@ -1,8 +1,10 @@
-#include <unistd.h>
+#include <unistd.h>                                                                                                                                                 
+
+
 
 int main(int ac,char **av)
 {
-	if (ac == 2)
+	if ( ac == 2)
 	{
 		int i = 0;
 		while (av[1][i])
@@ -13,7 +15,6 @@ int main(int ac,char **av)
 			}
 			if(av[1][i] == 'z' || av[1][i] == 'Z')
 				av[1][i] -= 25;
-
 			write(1,&av[1][i],1);
 			i++;
 		}
@@ -21,5 +22,4 @@ int main(int ac,char **av)
 	}
 	else
 		write(1,"\n",1);
-}
-
+} 
