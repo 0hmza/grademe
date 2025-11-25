e088ff6bf573772370f5af37b6457b6fd1ab84df
0hmza <hmza04@icloud.com>
2025-11-25 19:38:20 +0100

    ss

--- rendu/rotone/rotone.c
+++ rendu/rotone/rotone.c
@@ -10,11 +10,16 @@ int main(int ac,char **av)
 			if ((av[1][i] >= 'a' && av[1][i] <= 'y') || (av[1][i] >= 'A' && av[1][i] <= 'Y'))
 			{
 				av[1][i] = av[1][i] + 1;
+				write(1,&av[1][i],1);
 			}
-			if(av[1][i] == 'z' || av[1][i] == 'Z')
+			
+			else if(av[1][i] == 'z' || av[1][i] == 'Z')
+			{
 				av[1][i] -= 25;
-
-			write(1,&av[1][i],1);
+				write(1,&av[1][i],1);
+			}
+			else
+				write(1,&av[1][i],1);
 			i++;
 		}
 		write(1,"\n",1);
