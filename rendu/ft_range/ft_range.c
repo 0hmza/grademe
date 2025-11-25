2a85fb64dbb7db88fcea98f58355fb776f056d90
0hmza <hmza04@icloud.com>
2025-11-25 16:14:09 +0100

    f

---
+++ rendu/ft_range/ft_range.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int *ft_range(int start,int end)
+{
+	int i;
+	int *new;
+	int len;
+
+	i = 0;
+	len = end - start;
+	new = malloc(sizeof(int *) * len);
+	if (!new)
+		return 0;
+	if (start >= end)
+		return (0);
+	i = 0;
+	while (start < end)
+	{
+		new[i] = start;
+		i++;
+		start++;
+	}
+	return (new);
+}
