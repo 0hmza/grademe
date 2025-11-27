8b5802776f01e32ff5505dbf8fad15e83ff069dd
0hmza <hmza04@icloud.com>
2025-11-27 14:42:05 +0100

    union

---
+++ rendu/union/union.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+int main(int ac,char **av)
+{
+	char box[256] = {0};
+	int i = 0;
+	if (ac == 3)
+	{
+		while (av[1][i])
+		{
+			if (!box[(unsigned char)av[1][i]]++)
+				write(1,&av[1][i],1);
+			i++;
+		}
+		i = 0;
+		while (av[2][i])
+		{
+			if (!box[(unsigned char)av[2][i]]++)
+				write(1,&av[2][i],1);
+			i++;
+		}
+		write(1,"\n",1);
+	}
+	else
+		write(1,"\n",1);
+}
