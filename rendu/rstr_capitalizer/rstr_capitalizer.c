a59f69652774e5bb66a4fe18eed6089188068c89
0hmza <hmza04@icloud.com>
2025-11-27 17:02:23 +0100

    reverse Capita

--- rendu/rstr_capitalizer/rstr_capitalizer.c
+++ rendu/rstr_capitalizer/rstr_capitalizer.c
@@ -8,24 +8,30 @@ int check(char c)
 }
 int main(int ac,char **av)
 {
-	if (ac == 2)
+	if (ac > 1)
 	{
 		int i = 0;
-		while (av[1][i])
+		int j = 1;
+		while (j < ac)
 		{
-			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+			i  = 0;
+			while (av[j][i])
 			{
-				av[1][i] += 32;
+				if (av[j][i] >= 'A' && av[j][i] <= 'Z')
+				{
+					av[j][i] += 32;
+				}
+				if (check(av[j][i + 1]) == 1 || av[j][i + 1] == '\0')
+				{
+					if (av[j][i] >= 'a' && av[j][i] <= 'z')
+						av[j][i] -=32;
+				}
+				write(1,&av[j][i],1);
+				i++;
 			}
-			if (check(av[1][i + 1]) == 1 || av[1][i + 1] == '\0')
-			{
-				if (av[1][i] >= 'a' && av[1][i] <= 'z')
-					av[1][i] -=32;
-			}
-			write(1,&av[1][i],1);
-			i++;
+			write(1,"\n",1);
+			j++;
 		}
-		write(1,"\n",1);
 	}
 	else
 		write (1,"\n",1);
