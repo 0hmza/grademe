12ca6d3be7a4d5be0002a755c7ae434321f72d42
0hmza <hmza04@icloud.com>
2025-12-01 01:04:00 +0100

    fw

--- rendu/first_word/first_word.c
+++ rendu/first_word/first_word.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 int pause_t(char c)
 {
 	if (c == ' ' || c == '\t')
@@ -8,12 +9,30 @@ int pause_t(char c)
 void display(char *s)
 {
 	int i = 0;
-	while (pause_t(s[i]) == 1)
-		i++;
-	while (pause_t(s[i]) != 1)
+	if (pause_t(s[i]) == 1)
 	{
-		write(1,&s[i],1);
-		i++;
+		while (pause_t(s[i]) == 1)
+			i++;
+		while (s[i])
+		{
+			if (pause_t(s[i]) == 1 || s[i] == '\0')
+				break;
+			else
+				write(1,&s[i],1);
+			i++;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (s[i] != '\0')
+		{
+			if (pause_t(s[i]) == 1)
+				break;
+			else
+				write(1,&s[i],1);
+			i++;
+		}
 	}
 }
 int main(int ac,char **av)
