306ee5f5205d68ef680c944598994348448851b4
0hmza <hmza04@icloud.com>
2025-12-01 01:46:54 +0100

    hidenp

---
+++ rendu/hidenp/hidenp.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void display(char *s, char *s1)
+{
+	int i = 0;
+	int j = 0;
+	while (s[i] && s1[j])
+	{
+		if (s[i] == s1[j])
+			i++;
+		j++;
+	}
+	if (s[i] == '\0')
+		write(1,"1",1);
+	else
+		write(1,"0",1);
+}
+int main(int ac ,char **av)
+{
+	if (ac == 3)
+	{
+		display(av[1],av[2]);
+		write(1,"\n",1);
+	}
+	else
+		write(1,"\n",1);
+}
+
