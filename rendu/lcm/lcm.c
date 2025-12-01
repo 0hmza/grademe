514797468b556d45f530e971907f520139f8db67
0hmza <hmza04@icloud.com>
2025-12-01 15:11:22 +0100

    lcm

--- rendu/lcm/lcm.c
+++ rendu/lcm/lcm.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+#include <limits.h>
 int pgcd(unsigned int num1, unsigned int num2)
 {
-	int i = num1;
+	unsigned int i = num1;
 	while (i > 0)
 	{
 		if ((num1 % i == 0)  && (num2 % i == 0))
@@ -14,18 +14,15 @@ int pgcd(unsigned int num1, unsigned int num2)
 }
 unsigned int lcm(unsigned int a, unsigned int b)
 {
-	int total = 0;
-	if (a == 0 || b == 0)
-		return (0);
-	if (a > 0 && b > 0)
-	{
-		total = (a * b) / pgcd(a,b);
-	}
+	unsigned int total = 0;
+	if (a == 0 || b == 0 || a > INT_MAX || b > INT_MAX)
+		return (0); 
+	total = (a * b) / pgcd(a,b);
 	return total;
 }
-/*
+
 int main()
 {
-	printf("%d",lcm(42,4));
+	printf("%d",lcm(-4,2932));
 }
-*/
+
