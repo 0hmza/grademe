01c1dafa020257e3571e3aac36730ebefba3f0e2
0hmza <hmza04@icloud.com>
2025-12-01 16:43:11 +0100

    v2

---
+++ rendu/rostring/rostring.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+int main(int ac,char **av)
+{
+	int i = 0, j = 0;
+	if (ac > 1)
+	{
+		while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
+			i++;
+		j = i;
+		while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
+			i++;
+		int k = i;
+		int first = 1;
+		while (av[1][i])
+		{
+				while (av[1][i] == ' ' || av[1][i] == '\t')
+					i++;
+				if (!av[1][i])
+					break;
+				if (!first)
+					write(1," ",1);
+				first = 0;
+				int t = i;
+				while (av[1][t] && av[1][t] != ' ' && av[1][t] != '\t')
+					t++;
+				write(1,av[1] + i,t - i);
+				i = t;
+		}
+		if (k - j > 0)
+		{
+			if (!first)
+				write(1," ",1);
+		write(1,av[1] + j , k-j);
+		}
+	}
+	else
+		write(1,"\n",1);
+}
+
