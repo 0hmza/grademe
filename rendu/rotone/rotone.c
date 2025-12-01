080e144b07a117874ed4f48acde684c036d967f6
0hmza <hmza04@icloud.com>
2025-12-01 17:31:20 +0100

    v2

--- rendu/rotone/rotone.c
+++ rendu/rotone/rotone.c
@@ -1,22 +1,22 @@
 #include <unistd.h>
 
-int main(int ac,char **av)
+int main(int ac ,char **av)
 {
 	if (ac == 2)
 	{
-		int i = 0;
+		int i = 0 ;
 		while (av[1][i])
 		{
-			if ((av[1][i] >= 'a' && av[1][i] <= 'y') || (av[1][i] >= 'A' && av[1][i] <= 'Y'))
+			if ((av[1][i] >= 'a' && av[1][i] <= 'y')||(av[1][i] >= 'A' && av[1][i] <= 'Y'))
 			{
 				av[1][i] = av[1][i] + 1;
 				write(1,&av[1][i],1);
 			}
-			
-			else if(av[1][i] == 'z' || av[1][i] == 'Z')
+			else if (av[1][i] == 'z' || av[1][i] == 'Z')
 			{
-				av[1][i] -= 25;
+				av[1][i] = av[1][i] - 25;
 				write(1,&av[1][i],1);
+				
 			}
 			else
 				write(1,&av[1][i],1);
@@ -27,4 +27,3 @@ int main(int ac,char **av)
 	else
 		write(1,"\n",1);
 }
-
