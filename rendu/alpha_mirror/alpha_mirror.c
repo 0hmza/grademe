8da957071888106f95298b4f8e26a8f081b90623
0hmza <hmza04@icloud.com>
2025-12-02 12:49:57 +0100

    mirror

---
+++ rendu/alpha_mirror/alpha_mirror.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+void display(char *s)
+{
+	int i = 0;
+	int count = 0;
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = 'z' - (s[i] -'a');
+			write(1,&s[i],1);
+		}
+		else if (s[i] >= 'A' && s[i] <= 'Z')
+		{
+			s[i] = 'Z' - (s[i] - 'A');
+			write(1,&s[i],1);
+		}
+		else
+			write(1,&s[i],1);
+		i++;
+	}
+}
+int main(int ac,char **av)
+{
+	if (ac ==2 )
+	{
+		display(av[1]);
+		write(1,"\n",1);
+	}
+	else
+		write(1,"\n",1);
+}
