953ef2a7389bcd801dc49d1b02bb61eaf12785b7
0hmza <hmza04@icloud.com>
2025-12-02 13:45:06 +0100

    exp_str

---
+++ rendu/expand_str/expand_str.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+
+int check(char c)
+{
+	if (c == ' ' || c == '\t')
+		return 1;
+	return 0;
+}
+void display(char *s)
+{
+	int i = 0;
+	while (s[i])
+	{
+		if (check(s[i]) == 1)
+		{
+			int start = i;
+			while (check(s[i]) == 1)
+				i++;
+			int end = i;
+			while (start < end)
+			{
+				write(1,"   ",3);
+				break;
+			}
+		}
+		
+		else
+		{
+			write(1,&s[i],1);
+			i++;
+		}
+		
+	}
+			
+}
+int main(int ac,char **av)
+{
+	if (ac == 2)
+	{
+		display(av[1]);
+		write(1,"\n",1);
+	}
+	else
+		write(1,"\n",1);
+}
