c8b9d881b85c0158c951f4926778dc8dd6a536b3
0hmza <hmza04@icloud.com>
2025-12-02 15:11:42 +0100

    power2

--- rendu/is_power_of_2/is_power_of_2.c
+++ rendu/is_power_of_2/is_power_of_2.c
@@ -4,7 +4,7 @@
 int is_power_of_2(unsigned int n)
 {
 	unsigned int i = 0;
-	while (i < n)
+	while (i <= n)
 	{
 		if (i * i == n)
 			return 1;
@@ -16,7 +16,7 @@ int is_power_of_2(unsigned int n)
 int main()
 {
 	printf("%d\n",is_power_of_2(64));
-	printf("%d",is_power_of_2(17));
+	printf("%d",is_power_of_2(1));
 }
-*/
 
+*/
