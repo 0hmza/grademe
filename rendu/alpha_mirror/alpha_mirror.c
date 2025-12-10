730f3546c5fac307972a2628d0fc10e2e2cb737d
0hmza <hmza04@icloud.com>
2025-12-10 14:55:46 +0100

    vn

--- rendu/alpha_mirror/alpha_mirror.c
+++ rendu/alpha_mirror/alpha_mirror.c
@@ -1,31 +1,27 @@
 #include <unistd.h>
 
-void display(char *s)
-{
-	int i = 0;
-	int count = 0;
-	while (s[i])
-	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			s[i] = 'z' - (s[i] -'a');
-			write(1,&s[i],1);
-		}
-		else if (s[i] >= 'A' && s[i] <= 'Z')
-		{
-			s[i] = 'Z' - (s[i] - 'A');
-			write(1,&s[i],1);
-		}
-		else
-			write(1,&s[i],1);
-		i++;
-	}
-}
 int main(int ac,char **av)
 {
-	if (ac ==2 )
+	if (ac == 2)
 	{
-		display(av[1]);
+		int i = 0;
+		while (av[1][i])
+		{
+			
+			if (av[1][i] >= 'a' && av[1][i] <= 'z')
+			{
+				av[1][i] = 'z' - (av[1][i] - 'a');
+				write(1,&av[1][i],1);
+			}
+			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+			{
+				av[1][i] ='Z'- ( av[1][i] -'A');
+				write(1,&av[1][i],1);
+			}
+			else
+				write(1,&av[1][i],1);
+			i++;
+		}
 		write(1,"\n",1);
 	}
 	else
