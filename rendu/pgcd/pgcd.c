c7995877c301cf37c53ff3d821bc183dc94cd280
0hmza <hmza04@icloud.com>
2025-12-25 22:57:08 +0100

    d

--- rendu/pgcd/pgcd.c
+++ rendu/pgcd/pgcd.c
@@ -1,36 +1,47 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
-int pgcd(unsigned num, unsigned int num1)
+#include <limits.h>
+unsigned int pgcd(unsigned int num, unsigned int num1)
 {
-	unsigned int i;
+	unsigned int i = num;
 
-	i  = num;
-	while (num > 0)
+	while (i > 0)
 	{
-		if ((num % i == 0) && (num1 % i == 0))
+		if (num % i == 0 && num1 % i == 0)
 			return i;
 		i--;
 	}
-	return 0;
+	return 1;
 }
-
-int main(int ac,char **av)
+int ft_atoi(char *s)
 {
-	if ( ac == 3)
+	int i = 0, r = 0, d = 1;
+
+	while ((s[i] >= 9 && s[i] <= 13) || s[i] == ' ')
+		i++;
+	if (s[i] == '+' || s[i] == '-')
 	{
-		int num = atoi(av[1]);
-		int num1 = atoi(av[2]);
-		if (num < 0 || num1 < 0)
-			printf("0");
-		else
-		{
-			printf("%d",pgcd(atoi(av[1]),atoi(av[2])));
-			printf("\n");
-		}
+		if (s[i] == '-')
+			d = d * (-1);
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		r = r * 10 + s[i] - '0';
+		i++;
+	}
+	return d * r;
+}
+int main(int ac ,char **av)
+{
+	if (ac == 3)
+	{
+		int a = ft_atoi(av[1]);
+		int b = ft_atoi(av[2]);
+		if (a < 0 || b < 0 || a > INT_MAX || b > INT_MAX)
+			return 0;
+		printf("%d\n",pgcd(a,b));
 	}
 	else
-		printf("\n");
-		return 0;
+		write(1,"\n",1);
 }
-
