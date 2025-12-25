0bf716d9834f720fe29f234f15c3999282a23ba1
0hmza <hmza04@icloud.com>
2025-12-25 23:16:25 +0100

    d

--- rendu/rostring/rostring.c
+++ rendu/rostring/rostring.c
@@ -1,41 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
-
-int main(int ac,char **av)
+char **ft_split(char *s)
 {
-	int i = 0, j = 0;
-	if (ac > 1)
+	int i = 0,j = 0, k =0;
+	char **box = malloc(sizeof(char *) * 2096);
+	if (!box)
+		return NULL;
+	while (s[i])
 	{
-		while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
+		while (s[i] && s[i] <= 32)
 			i++;
-		j = i;
-		while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
+		if (!s[i])
+			break;
+		box[j] = malloc(4096);
+		if (!box[j])
+			return NULL;
+		k = 0;
+		while (s[i] && s[i] > 32)
+			box[j][k++] = s[i++];
+		box[j][k] = '\0';
+		j++;
+	}
+	box[j] = NULL;
+	return box;
+}
+int ft_strlen(char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+	return i;
+}
+int main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		char **r = ft_split(av[1]);
+		int i = 1;
+		while (r[i])
+		{
+			write(1,r[i],ft_strlen(r[i]));
+			write(1,(" "),1);
 			i++;
-		int k = i;
-		int first = 1;
-		while (av[1][i])
-		{
-				while (av[1][i] == ' ' || av[1][i] == '\t')
-					i++;
-				if (!av[1][i])
-					break;
-				if (!first)
-					write(1," ",1);
-				first = 0;
-				int t = i;
-				while (av[1][t] && av[1][t] != ' ' && av[1][t] != '\t')
-					t++;
-				write(1,av[1] + i,t - i);
-				i = t;
 		}
-		if (k - j > 0)
-		{
-			if (!first)
-				write(1," ",1);
-		write(1,av[1] + j , k-j);
-		}
-		write(1,"\n",1);
+		write(1,r[0],ft_strlen(r[0]));
 	}
 	else
 		write(1,"\n",1);
 }
-
