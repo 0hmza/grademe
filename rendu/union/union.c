dfeadee5e3cd7c532a9f895a411de95d06adb2d1
0hmza <hmza04@icloud.com>
2025-12-26 13:09:03 +0100

    d

--- rendu/union/union.c
+++ rendu/union/union.c
@@ -2,25 +2,27 @@
 
 int main(int ac,char **av)
 {
-	char box[256] = {0};
-	int i = 0;
 	if (ac == 3)
 	{
+		char box[256] = {0};
+		int i = 0;
 		while (av[1][i])
 		{
 			if (!box[(unsigned char)av[1][i]]++)
-				write(1,&av[1][i],1);
+				write(1,&av[1][i],1);	
 			i++;
 		}
-		i = 0;
-		while (av[2][i])
+		int j = 0;
+		while (av[2][j])
 		{
-			if (!box[(unsigned char)av[2][i]]++)
-				write(1,&av[2][i],1);
-			i++;
+			if (!box[(unsigned char)av[2][j]]++)
+				write(1,&av[2][j],1);
+			j++;
 		}
 		write(1,"\n",1);
 	}
 	else
 		write(1,"\n",1);
 }
+
+
